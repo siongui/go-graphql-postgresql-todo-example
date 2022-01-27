f69da44d21d729525df38b1eacab81c82dd25b95
siongui <siongui@gmail.com>
2022-01-28 02:28:58 +0800

    add reference

--- tools.go
+++ tools.go
@@ -6,3 +6,6 @@ package tools
 import (
 	_ "github.com/99designs/gqlgen"
 )
+
+// For purpose of this file, please see
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
